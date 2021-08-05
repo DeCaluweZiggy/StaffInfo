e384296aeb6fdbfbda2427aa1a1f7112456ec490
Ziggy De Caluwé <ziggy.de.caluwe@student.howest.be>
2021-08-05 17:29:41 +0200

    Add interface IPerson + Enum contract

---
+++ Staff_Information/StaffMembers.Core/Enums/Contract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaffMembers.Core.Enums
+{
+    public enum Contract
+    {
+        OnbepaaldeDuur, bepaaldeDuur, Interiem
+    }
+}
---
+++ Staff_Information/StaffMembers.Core/Interfaces/IPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace StaffMembers.Core.Interfaces
+{
+    public interface IPerson
+    {
+        Guid Id { get; }
+
+        string FirtName { get; }
+
+        string LastName { get;}
+
+        string Adress { get; }
+
+        string AccountNumber { get; }
+
+        int HoursAWeek { get; set; }
+
+        Contract Contract { get; set; }
+        int OverTime { get; set; }
+    }
+}
